a167395708fdd2df62e1e337bb6d1dc155b84969
Elizabeth <i_am_exlusive@mail.ru>
2023-05-10 21:57:34 +0300

    no message

---
+++ apps/login/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from .views import LoginView
+
+urlpatterns = [
+    path('account/', views.user_account, name='user_account'),
+    path('account_setting/', views.account_setting, name='account_setting'),
+    path('friends/', views.friends, name='friends'),
+    path('friend_request/', views.friend_request, name='friend_request'),
+    path('id<int:account_id>/add_friend', views.add_friend, name='add_friend'),
+    path('id<int:account_id>/add_follower', views.add_follower, name='add_follower'),
+    path('confirm_friend<int:account_id>/', views.confirm_friend, name='confirm_friend'),
+    path('delete_friend<int:account_id>/', views.delete_friend, name='delete_friend'),
+    path('delete_follower<int:account_id>/', views.delete_follower, name='delete_follower'),
+    path('users/', views.find_users, name='find_users'),
+    path('id<int:account_id>/', views.account, name='account'),
+    path('', views.index, name='index'),
+    path('login/', standart_views.LoginView.as_view(), name='login'),
+    path('logout/', standart_views.LogoutView.as_view(), name='logout'),
+    path('registration/', views.register, name='register'),
+
+]
